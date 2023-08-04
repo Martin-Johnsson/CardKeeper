a93ecb073c1411e0e6d71867985a89b54f62cfbb
Martin Johnsson <maartinjohnsson@gmail.com>
2023-08-04 13:41:40 +0200

    Reorganize routes and routesArray and rename pages and routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { routes } from './routes.tsx';
+import { routes } from './routes/routesArray.tsx';
 
 const router = createBrowserRouter(routes);
 ReactDOM.createRoot(document.getElementById('root')!).render(
--- src/routes/routesArray.tsx
+++ src/routes/routesArray.tsx
@@ -1,4 +1,4 @@
-import LandingPage from './pages/LandingPage';
+import LandingPage from './LandingPage';
 
 export const routes = [
   {
